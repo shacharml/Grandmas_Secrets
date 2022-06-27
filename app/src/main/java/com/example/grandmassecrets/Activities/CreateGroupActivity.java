6f5580f718735ce852e6081d120d97e45da7208f
shacharml <shacharml611@gmail.com>
2022-06-27 15:30:25 +0300

    Work on view Pages

---
+++ app/src/main/java/com/example/grandmassecrets/Activities/CreateGroupActivity.java
@@ -0,0 +1,16 @@
+package com.example.grandmassecrets.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.grandmassecrets.R;
+
+public class CreateGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group);
+    }
+}
