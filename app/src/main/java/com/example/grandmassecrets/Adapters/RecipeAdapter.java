dfbf57088ad7676abf4cc6b53431da0022f0cdb5
shacharml <shacharml611@gmail.com>
2022-06-29 12:31:28 +0300

    Add Recipe Adapter

---
+++ app/src/main/java/com/example/grandmassecrets/Adapters/RecipeAdapter.java
@@ -0,0 +1,134 @@
+package com.example.grandmassecrets.Adapters;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.widget.AppCompatImageView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.grandmassecrets.Objects.NutritionFacts;
+import com.example.grandmassecrets.Objects.Recipe;
+import com.example.grandmassecrets.R;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.ArrayList;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        public interface RecipeListener {
+                void recipeClicked(Recipe recipe, int position);
+        }
+
+    private Activity activity;
+    private ArrayList<Recipe> recipes = new ArrayList<>();
+    private RecipeListener listener;
+
+    //Constructor
+    public RecipeAdapter(Activity activity, ArrayList<Recipe> Recipes) {
+        this.activity = activity;
+        this.recipes = Recipes;
+    }
+
+    public RecipeAdapter setListener(RecipeListener listener) {
+        this.listener = listener;
+        return this;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_item_card, parent, false);
+        return new RecipeHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+
+        final RecipeHolder holder = (RecipeHolder) viewHolder;
+        //get the recipe in the specific position
+        Recipe recipe = getRecipe(position);
+        //Update the ui
+        holder.recipe_item_LBL_title.setText(recipe.getName());
+        holder.recipe_item_LBL_sub_title.setText(recipe.getDescription());
+
+        // Get the image of the recipe dish from Data Storage WIth Glide
+            // TODO: 29/06/2022 Check if glide work with thr uri/l image
+        Glide.with(activity).load(recipe.getImg()).into(holder.recipe_item_IMG_img);
+
+        //get the Images of the nutrition facts (not from data Storage
+        for (NutritionFacts facts : recipe.getNutritionFacts()) {
+            switch (facts.getNameNutrition()) {
+                case ORGANIC:
+                    holder.nutrition_IMG_nut1.setImageResource(R.drawable.ic_organic_food);
+                    holder.nutrition_IMG_nut1.setVisibility(View.VISIBLE);
+                    break;
+                case CARBOHYDRATES:
+                    holder.nutrition_IMG_nut2.setImageResource(R.drawable.ic_carbohydrates);
+                    holder.nutrition_IMG_nut2.setVisibility(View.VISIBLE);
+                    break;
+                case DAIRY:
+                    holder.nutrition_IMG_nut3.setImageResource(R.drawable.ic_dairy);
+                    holder.nutrition_IMG_nut3.setVisibility(View.VISIBLE);
+                    break;
+                case NO_EGG:
+                    holder.nutrition_IMG_nut4.setImageResource(R.drawable.ic_no_eggs);
+                    holder.nutrition_IMG_nut4.setVisibility(View.VISIBLE);
+                    break;
+                case NO_PEANUT:
+                    holder.nutrition_IMG_nut5.setImageResource(R.drawable.ic_no_peanut);
+                    holder.nutrition_IMG_nut5.setVisibility(View.VISIBLE);
+                    break;
+            }
+        }
+
+//                Check if don't work otherwise
+//                int resourceId = activity.getResources().getIdentifier(recipe.getImage(), "drawable", activity.getPackageName());
+//                holder.Recipe_IMG_image.setImageResource(resourceId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes.size();
+    }
+
+    // Get the recipe in the specific position
+    public Recipe getRecipe(int position) {
+        return recipes.get(position);
+    }
+
+    // External class - HOLDER
+    class RecipeHolder extends RecyclerView.ViewHolder {
+
+        private ShapeableImageView recipe_item_IMG_img;
+        private MaterialTextView recipe_item_LBL_title;
+        private MaterialTextView recipe_item_LBL_sub_title;
+        private AppCompatImageView nutrition_IMG_nut1;
+        private AppCompatImageView nutrition_IMG_nut2;
+        private AppCompatImageView nutrition_IMG_nut3;
+        private AppCompatImageView nutrition_IMG_nut4;
+        private AppCompatImageView nutrition_IMG_nut5;
+
+
+        public RecipeHolder(View itemView) {
+            super(itemView);
+
+            recipe_item_IMG_img = itemView.findViewById(R.id.recipe_item_IMG_img);
+            recipe_item_LBL_title = itemView.findViewById(R.id.recipe_item_LBL_title);
+            recipe_item_LBL_sub_title = itemView.findViewById(R.id.recipe_item_LBL_sub_title);
+            nutrition_IMG_nut1 = itemView.findViewById(R.id.nutrition_IMG_nut1);
+            nutrition_IMG_nut2 = itemView.findViewById(R.id.nutrition_IMG_nut2);
+            nutrition_IMG_nut3 = itemView.findViewById(R.id.nutrition_IMG_nut3);
+            nutrition_IMG_nut4 = itemView.findViewById(R.id.nutrition_IMG_nut4);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    listener.recipeClicked(getRecipe(getAdapterPosition()), getAdapterPosition());
+                }
+            });
+        }
+    }
+}
