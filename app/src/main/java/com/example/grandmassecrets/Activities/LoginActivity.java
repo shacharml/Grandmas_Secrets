25823460b76979c6b5e52484b6c86b6c6f5b7b86
shacharml <shacharml611@gmail.com>
2022-06-30 14:27:59 +0300

    add if user exist option

--- app/src/main/java/com/example/grandmassecrets/Activities/LoginActivity.java
+++ app/src/main/java/com/example/grandmassecrets/Activities/LoginActivity.java
@@ -1,8 +1,5 @@
 package com.example.grandmassecrets.Activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,6 +8,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.grandmassecrets.R;
 import com.google.firebase.FirebaseException;
@@ -29,6 +29,49 @@ public class LoginActivity extends AppCompatActivity {
 
     // variable for FirebaseAuth class
     private FirebaseAuth myAuth;
+    // callback method is called on Phone auth provider.
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            // initializing our callbacks for on verification callback method.
+            mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        /**
+         *    below method is used when OTP is sent from Firebase
+         *    after the code sent Moving to Verify Page
+         */
+        @Override
+        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            login_BTN_send.setVisibility(View.VISIBLE);
+            login_PRB_progress_bar.setVisibility(View.GONE);
+            Toast.makeText(LoginActivity.this, "OTP is successfully send.", Toast.LENGTH_SHORT).show();
+
+            // Move to verify activity
+            Intent intent = new Intent(LoginActivity.this, VerifyActivity.class);
+            intent.putExtra(getString(R.string.LBL_phone), login_EDT_phone_number.getText().toString().trim()); //pass the phone number
+            intent.putExtra(getString(R.string.LBL_verification_Id), s); //pass the unique id
+            startActivity(intent);
+            finish();
+
+        }
+
+        // this method is called when user receive OTP from Firebase.
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+            login_BTN_send.setVisibility(View.GONE);
+            login_PRB_progress_bar.setVisibility(View.VISIBLE);
+        }
+
+        /**
+         this method is called when firebase doesn't
+         sends our OTP code due to any error or issue.
+         */
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            // displaying error message with firebase exception.
+            login_PRB_progress_bar.setVisibility(View.GONE);
+            login_BTN_send.setVisibility(View.VISIBLE);
+            Toast.makeText(LoginActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +85,10 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // Check not Empty
                 if (TextUtils.isEmpty(login_EDT_phone_number.getText().toString()))
-                    Toast.makeText(LoginActivity.this,"Invalid phone number",Toast.LENGTH_SHORT).show();
-                // Check valid phone number  - length
+                    Toast.makeText(LoginActivity.this, "Invalid phone number", Toast.LENGTH_SHORT).show();
+                    // Check valid phone number  - length
                 else if (login_EDT_phone_number.getText().toString().length() != 9)
-                             Toast.makeText(LoginActivity.this, "Type valid Phone Number", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Type valid Phone Number", Toast.LENGTH_SHORT).show();
                 else {
                     login_PRB_progress_bar.setVisibility(View.VISIBLE); // Show progress bar
                     String number = login_EDT_phone_number.getText().toString(); // Get the phone number that insert
@@ -62,7 +105,7 @@ public class LoginActivity extends AppCompatActivity {
     private void sendVerificationCode(String number) {
         PhoneAuthOptions options =
                 PhoneAuthOptions.newBuilder(myAuth)
-                        .setPhoneNumber(getString(R.string.IL_num) +number)      // Phone number to verify
+                        .setPhoneNumber(getString(R.string.IL_num) + number)      // Phone number to verify
                         .setTimeout(60L, TimeUnit.SECONDS)    // Timeout and unit
                         .setActivity(this)                          // Activity (for callback binding)
                         .setCallbacks(mCallBack)                  // OnVerificationStateChangedCallbacks
@@ -70,65 +113,10 @@ public class LoginActivity extends AppCompatActivity {
         PhoneAuthProvider.verifyPhoneNumber(options);
     }
 
-    // callback method is called on Phone auth provider.
-    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
-            // initializing our callbacks for on verification callback method.
-            mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
-
-        // below method is used when OTP is sent from Firebase
-        @Override
-        public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-
-            login_PRB_progress_bar.setVisibility(View.GONE);
-            Toast.makeText(LoginActivity.this, "OTP is successfully send.", Toast.LENGTH_SHORT).show();
-
-            // Move to verify activity
-            Intent intent = new Intent(LoginActivity.this, VerifyActivity.class);
-            intent.putExtra(getString(R.string.LBL_phone),  login_EDT_phone_number.getText().toString().trim());
-            intent.putExtra(getString(R.string.LBL_verification_Id), s);
-            startActivity(intent);
-            finish();
-//            super.onCodeSent(s, forceResendingToken);
-//            // when we receive the OTP it contains a unique id which
-//            // we are storing in our string which we have already created.
-//            verificationId = s;
-        }
-
-        // this method is called when user receive OTP from Firebase.
-        @Override
-        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
-//            // getting OTP code which is sent in phone auth credentials.
-//            code = phoneAuthCredential.getSmsCode();
-//
-//            // checking if the code
-//            // is null or not.
-//            if (code != null) {
-//
-//                // TODO: 23/06/2022 need to move data to verify the code
-//                //need to move verify layout
-//                Intent intent = new Intent(LoginActivity.this,VerifyActivity.class);
-//                startActivity(intent);
-//
-//            }
-        }
-
-        /*
-             this method is called when firebase doesn't
-             sends our OTP code due to any error or issue.
-         */
-        @Override
-        public void onVerificationFailed(FirebaseException e) {
-            // displaying error message with firebase exception.
-            login_PRB_progress_bar.setVisibility(View.GONE);
-            Toast.makeText(LoginActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-        }
-    };
-
-
     private void findViews() {
         login_EDT_phone_number = findViewById(R.id.login_EDT_phone_number);
         login_BTN_send = findViewById(R.id.login_BTN_send);
-        login_PRB_progress_bar = findViewById(R.id.login_PRB_progress_bar);
+        login_PRB_progress_bar = findViewById(R.id.verify_PRB_progress_bar);
         myAuth = FirebaseAuth.getInstance();
     }
 
@@ -136,10 +124,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
     }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
     }
+
     @Override
     protected void onResume() {
         super.onResume();
--- app/src/main/java/com/example/grandmassecrets/Activities/VerifyActivity.java
+++ app/src/main/java/com/example/grandmassecrets/Activities/VerifyActivity.java
@@ -1,8 +1,5 @@
 package com.example.grandmassecrets.Activities;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -10,27 +7,42 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.grandmassecrets.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.concurrent.TimeUnit;
 
 public class VerifyActivity extends AppCompatActivity {
 
-    private EditText verify_EDT_code1,verify_EDT_code2,verify_EDT_code3,verify_EDT_code4,verify_EDT_code5,verify_EDT_code6;
+    private EditText verify_EDT_code1, verify_EDT_code2, verify_EDT_code3, verify_EDT_code4, verify_EDT_code5, verify_EDT_code6;
     private TextView verify_TXT_text_mobile;
-    private Button verify_BTN_send;
+    private TextView verify_TXT_resend;
+    private Button verify_BTN_verify;
+    private ProgressBar verify_PRB_progress_bar;
 
     // string for storing our verification ID
     private String verificationId;
+    private String phoneNumber;
     private FirebaseAuth myAuth;
-//    private String code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,17 +51,24 @@ public class VerifyActivity extends AppCompatActivity {
 
         findViews();
         setupInputs();
-        verify_TXT_text_mobile.setText(String.format(
-                "+972%s",getIntent().getStringExtra(getString(R.string.LBL_phone))
-        ));
+        phoneNumber = String.format("+972%s", getIntent().getStringExtra(getString(R.string.LBL_phone)));
+        verify_TXT_text_mobile.setText(phoneNumber);
 
-
+        // Get the Unique Id from the login Activity
         verificationId = getIntent().getStringExtra(getString(R.string.LBL_verification_Id));
 
-        verify_BTN_send.setOnClickListener(new View.OnClickListener() {
+        //Resend OTP
+        verify_TXT_resend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                progressBarVerify.setVisibility(View.VISIBLE);
+                sendVerificationCode(phoneNumber);
+            }
+        });
+
+        //Verify the OTP inserted
+        verify_BTN_verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 if (verify_EDT_code1.getText().toString().trim().isEmpty() ||
                         verify_EDT_code2.getText().toString().trim().isEmpty() ||
                         verify_EDT_code3.getText().toString().trim().isEmpty() ||
@@ -57,20 +76,23 @@ public class VerifyActivity extends AppCompatActivity {
                         verify_EDT_code5.getText().toString().trim().isEmpty() ||
                         verify_EDT_code6.getText().toString().trim().isEmpty()) {
                     Toast.makeText(VerifyActivity.this, "OTP is not Valid!", Toast.LENGTH_SHORT).show();
-                }
-                else {
+                } else {
                     if (verificationId != null) {
-                        String code = getCode();
+                        verify_BTN_verify.setVisibility(View.GONE);
+                        verify_PRB_progress_bar.setVisibility(View.VISIBLE);
+                        String code = getCodeOTP();
                         PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
                         signInWithCredential(credential);
                     }
+                    else
+                        Toast.makeText(VerifyActivity.this, "please check internet connection", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    private String getCode() {
-       return verify_EDT_code1.getText().toString().trim() +
+    private String getCodeOTP() {
+        return verify_EDT_code1.getText().toString().trim() +
                 verify_EDT_code2.getText().toString().trim() +
                 verify_EDT_code3.getText().toString().trim() +
                 verify_EDT_code4.getText().toString().trim() +
@@ -84,30 +106,31 @@ public class VerifyActivity extends AppCompatActivity {
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        verify_BTN_verify.setVisibility(View.VISIBLE);
+                        verify_PRB_progress_bar.setVisibility(View.GONE);
+
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Toast.makeText(VerifyActivity.this, "Authentication Success", Toast.LENGTH_LONG).show();
 
-                            // Now need to check if the user is already in the database or need to sign up
-
-
-
-
-
-//                            FirebaseUser user = task.getResult().getUser();
-//                            // Update UI
-
-                            // TODO: 23/06/2022 this is when it secsesful - we need to forward the uid that came back and check if already exist enter to application else send to sign up activity
-//                binding.progressBarVerify.setVisibility(View.VISIBLE);
-//                binding.btnVerify.setVisibility(View.INVISIBLE);
-                            // if the code is correct and the task is successful
-                            // we are sending our user to new activity.
-                            Intent i = new Intent(VerifyActivity.this, MainActivity.class);
-                            startActivity(i);
-                            finish();
+                            /** Now need to check if the user is already in the database or need to sign up */
+                            boolean isNew = task.getResult().getAdditionalUserInfo().isNewUser();
+                            if(isNew){
+                                // Move to verify activity
+                                Intent intent = new Intent(VerifyActivity.this, SignUpActivity.class);
+                                intent.putExtra(getString(R.string.LBL_phone), phoneNumber); //pass the phone number
+                                startActivity(intent);
+                                finish();
+                            }else {
+                                // if the code is correct and the user exist
+                                Intent i = new Intent(VerifyActivity.this, MainActivity.class);
+                                startActivity(i);
+                                finish();
+                            }
                         } else {
                             // if the code is not correct then we are displaying an error message to the user.
-                            Toast.makeText(VerifyActivity.this, "Authentication Failed", Toast.LENGTH_LONG).show();
+                            Toast.makeText(VerifyActivity.this, "Authentication Failed - Enter the Correct OTP", Toast.LENGTH_LONG).show();
                         }
                     }
                 });
@@ -120,46 +143,44 @@ public class VerifyActivity extends AppCompatActivity {
         verify_EDT_code4 = findViewById(R.id.verify_EDT_code4);
         verify_EDT_code5 = findViewById(R.id.verify_EDT_code5);
         verify_EDT_code6 = findViewById(R.id.verify_EDT_code6);
+        verify_TXT_resend = findViewById(R.id.verify_TXT_resend);
         verify_TXT_text_mobile = findViewById(R.id.verify_TXT_text_mobile);
-        verify_BTN_send = findViewById(R.id.create_group_BTN_save);
-        myAuth =  FirebaseAuth.getInstance();
+        verify_BTN_verify = findViewById(R.id.verify_BTN_verify);
+        verify_PRB_progress_bar = findViewById(R.id.verify_PRB_progress_bar);
+        myAuth = FirebaseAuth.getInstance();
     }
 
-    private void setupInputs(){
+    private void setupInputs() {
         verify_EDT_code1.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(!charSequence.toString().trim().isEmpty()){
+                if (!charSequence.toString().trim().isEmpty()) {
                     verify_EDT_code2.requestFocus();
                 }
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-
             }
         });
         verify_EDT_code2.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
             }
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(!charSequence.toString().trim().isEmpty()){
+                if (!charSequence.toString().trim().isEmpty()) {
                     verify_EDT_code3.requestFocus();
                 }
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
-
             }
         });
         verify_EDT_code3.addTextChangedListener(new TextWatcher() {
@@ -170,7 +191,7 @@ public class VerifyActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(!charSequence.toString().trim().isEmpty()){
+                if (!charSequence.toString().trim().isEmpty()) {
                     verify_EDT_code4.requestFocus();
                 }
             }
@@ -188,7 +209,7 @@ public class VerifyActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(!charSequence.toString().trim().isEmpty()){
+                if (!charSequence.toString().trim().isEmpty()) {
                     verify_EDT_code5.requestFocus();
                 }
             }
@@ -206,7 +227,7 @@ public class VerifyActivity extends AppCompatActivity {
 
             @Override
             public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(!charSequence.toString().trim().isEmpty()){
+                if (!charSequence.toString().trim().isEmpty()) {
                     verify_EDT_code6.requestFocus();
                 }
             }
@@ -218,16 +239,60 @@ public class VerifyActivity extends AppCompatActivity {
         });
     }
 
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+            // initializing our callbacks for on verification callback method.
+            mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+        /**
+         *    below method is used when OTP is sent from Firebase
+         *    after the code sent Moving to Verify Page
+         */
+        @Override
+        public void onCodeSent(@NonNull String newS, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+            verificationId = newS;
+            Toast.makeText(VerifyActivity.this, "OTP is successfully send.", Toast.LENGTH_SHORT).show();
+        }
+
+        // this method is called when user receive OTP from Firebase.
+        @Override
+        public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+        }
+
+        /**
+         this method is called when firebase doesn't
+         sends our OTP code due to any error or issue.
+         */
+        @Override
+        public void onVerificationFailed(FirebaseException e) {
+            Toast.makeText(VerifyActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+        }
+    };
+
+    /**
+       this method is used for getting
+       OTP (code) on user phone number.
+    */
+    private void sendVerificationCode(String number) {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(myAuth)
+                        .setPhoneNumber(number)      // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS)    // Timeout and unit
+                        .setActivity(this)                          // Activity (for callback binding)
+                        .setCallbacks(mCallBack)                  // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
 
     @Override
     protected void onResume() {
         super.onResume();
     }
+
     @Override
     protected void onStop() {
         super.onStop();
-//        code = getCode();
     }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
