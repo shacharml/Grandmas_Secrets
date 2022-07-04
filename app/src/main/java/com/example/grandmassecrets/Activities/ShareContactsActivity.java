f40967d1c0b13554c21a1f595302f8b828a0f135
shacharml <shacharml611@gmail.com>
2022-07-04 11:29:01 +0300

    test

--- app/src/main/java/com/example/grandmassecrets/Activities/ShareContactsActivity.java
+++ app/src/main/java/com/example/grandmassecrets/Activities/ShareContactsActivity.java
@@ -12,5 +12,6 @@ public class ShareContactsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_contacs);
+        //new
     }
 }
